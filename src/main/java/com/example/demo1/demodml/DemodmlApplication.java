7a5dac9b6b256cbc7ee21e53abe2a723455c6a95
vinnycrm <vinnycrm@gmail.com>
2026-07-27 17:34:40 +0530

    intial commit

---
+++ src/main/java/com/example/demo1/demodml/DemodmlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo1.demodml;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodmlApplication.class, args);
+	}
+
+}
